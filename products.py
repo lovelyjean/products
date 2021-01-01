f26a4fbb306dd2c80a5ee76c618831f36a2c2ff5
lovelyjean <lovely.jean@gmail.com>
2021-01-01 20:53:59 +0900

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,13 @@
+
 products = []
+
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
 
 while True:
 	name = input('product name: ')
@@ -19,7 +28,7 @@ for product in products:
 	print(product[0], '的價格是', product[1])
 
 with open('products.csv', 'w', encoding='utf-8') as f:
-# with open('products.csv', 'w') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '\n')
+
