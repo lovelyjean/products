2916ea1aba50f02a1f2949477678682930d6369d
lovelyjean <lovely.jean@gmail.com>
2021-01-01 18:59:04 +0900

    add hearde

--- products.py
+++ products.py
@@ -17,3 +17,9 @@ print(products)
 
 for product in products:
 	print(product[0], '的價格是', product[1])
+
+with open('products.csv', 'w', encoding='utf-8') as f:
+# with open('products.csv', 'w') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
